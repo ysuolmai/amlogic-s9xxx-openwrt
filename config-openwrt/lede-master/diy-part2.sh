625cf81ebda1acf3cbc12d605fef6864370a3771
ysuolmai <fan0417@gmail.com>
2023-05-06 10:54:59 +0800

    Update diy-part2.sh

--- config-openwrt/lede-master/diy-part2.sh
+++ config-openwrt/lede-master/diy-part2.sh
@@ -8,6 +8,11 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+#replace argon theme
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
