3d58369bab683c1a0f4228fd20162f2ec0f5caf5
ysuolmai <fan0417@gmail.com>
2025-03-02 13:12:44 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -29,103 +29,214 @@ rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
 
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+#安装和更新软件包
+UPDATE_PACKAGE() {
+	local PKG_NAME=$1
+	local PKG_REPO=$2
+	local PKG_BRANCH=$3
+	local PKG_SPECIAL=$4
+
+	# 清理旧的包
+	read -ra PKG_NAMES <<< "$PKG_NAME"  # 将PKG_NAME按空格分割成数组
+	for NAME in "${PKG_NAMES[@]}"; do
+		rm -rf $(find feeds/luci/ feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" -prune)
+	done
+
+	# 克隆仓库
+	if [[ $PKG_REPO == http* ]]; then
+		local REPO_NAME=$(echo $PKG_REPO | awk -F '/' '{gsub(/\.git$/, "", $NF); print $NF}')
+		git clone --depth=1 --single-branch --branch $PKG_BRANCH "$PKG_REPO" package/$REPO_NAME
+	else
+		local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
+		git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git" package/$REPO_NAME
+	fi
+
+	# 根据 PKG_SPECIAL 处理包
+	case "$PKG_SPECIAL" in
+		"pkg")
+			# 提取每个包
+			for NAME in "${PKG_NAMES[@]}"; do
+   				echo "moving $NAME"
+				cp -rf $(find ./package/$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$NAME*" -prune) ./package/
+			done
+			# 删除剩余的包
+			rm -rf ./package/$REPO_NAME/
+			;;
+		"name")
+			# 重命名包
+			mv -f ./package/$REPO_NAME ./package/$PKG_NAME
+			;;
+	esac
+}
+
+
+# 添加额外插件
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/VIKINGYFY/homeproxy package/homeproxy
+
+# 科学上网插件
+#UPDATE_PACKAGE "homeproxy" "https://github.com/VIKINGYFY/homeproxy.git" "main"
+#UPDATE_PACKAGE "luci-app-adguardhome" "https://github.com/ysuolmai/luci-app-adguardhome.git" "master"
+# Themes
+#UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
+
+
+
+#DDNS-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
 #luci-app-zerotier
 git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
 
-#luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
+#tailscale
+#sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+#git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 
+#gecoosac
+#git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+
+#lucky
+#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+
+#alist
+UPDATE_PACKAGE "alist" "https://github.com/sbwml/luci-app-alist.git" "main"
+
+#small-package
+UPDATE_PACKAGE "xray-core xray-plugin dns2tcp dns2socks haproxy hysteria \
+        naiveproxy shadowsocks-rust v2ray-core v2ray-geodata v2ray-geoview v2ray-plugin \
+        tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev \
+        luci-app-passwall smartdns luci-app-smartdns v2dat mosdns luci-app-mosdns \
+        taskd luci-lib-xterm luci-lib-taskd \
+        luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
+        luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash mihomo \
+        luci-app-nikki" "kenzok8/small-package" "main" "pkg"
+
+#speedtest
+UPDATE_PACKAGE "luci-app-netspeedtest" "https://github.com/sbwml/openwrt_pkgs.git" "main" "pkg"
+UPDATE_PACKAGE "speedtest-cli" "https://github.com/sbwml/openwrt_pkgs.git" "main" "pkg"
+
+UPDATE_PACKAGE "luci-app-adguardhome" "https://github.com/ysuolmai/luci-app-adguardhome.git" "master"
+UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 keywords_to_delete=(
-"passwall"
-"v2ray"
-"sing-box"
-"ddns"
-"SINGBOX"
-"openvpn"
-"uugamebooster"
-#"luci-app-homeproxy"
-"CONFIG_TARGET_INITRAMFS"
-"tailscale"
+    "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
+    "smartdns" "kucat" "bootstrap"
 )
 
-for line in "${keywords_to_delete[@]}"; do
-    sed -i "/$line/d" ./.config
+
+for keyword in "${keywords_to_delete[@]}"; do
+    sed -i "/$keyword/d" ./.config
 done
 
+# Configuration lines to append to .config
 provided_config_lines=(
-#"CONFIG_PACKAGE_luci-app-ssr-plus=y"
-#"CONFIG_PACKAGE_luci-i18n-ssr-plus-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-zerotier=y"
-"CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-adguardhome=y"
-"CONFIG_PACKAGE_luci-i18n-adguardhome-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-ddns-go=y"
-"CONFIG_PACKAGE_luci-i18n-ddns-go-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-poweroff=y"
-"CONFIG_PACKAGE_luci-i18n-poweroff-zh-cn=y"
-"CONFIG_PACKAGE_cpufreq=y"
-"CONFIG_PACKAGE_luci-app-cpufreq=y"
-"CONFIG_PACKAGE_luci-i18n-cpufreq-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-ttyd=y"
-"CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"
-"CONFIG_PACKAGE_ttyd=y"
-#"CONFIG_PACKAGE_luci-app-passwall=y"
-#"CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y"
-"CONFIG_PACKAGE_luci-app-homeproxy=y"
-"CONFIG_PACKAGE_luci-i18n-homeproxy-zh-cn=y"
-"CONFIG_PACKAGE_luci-theme-argon=y"
-"CONFIG_PACKAGE_luci-app-argon-config=y"
+    "CONFIG_PACKAGE_luci-app-zerotier=y"
+    "CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-adguardhome=y"
+    "CONFIG_PACKAGE_luci-i18n-adguardhome-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-poweroff=y"
+    "CONFIG_PACKAGE_luci-i18n-poweroff-zh-cn=y"
+    "CONFIG_PACKAGE_cpufreq=y"
+    "CONFIG_PACKAGE_luci-app-cpufreq=y"
+    "CONFIG_PACKAGE_luci-i18n-cpufreq-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-ttyd=y"
+    "CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"
+    "CONFIG_PACKAGE_ttyd=y"
+    "CONFIG_PACKAGE_luci-app-homeproxy=y"
+    "CONFIG_PACKAGE_luci-i18n-homeproxy-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-ddns-go=y"
+    "CONFIG_PACKAGE_luci-i18n-ddns-go-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-argon-config=y"
+    "CONFIG_PACKAGE_nano=y"
+    "CONFIG_BUSYBOX_CONFIG_LSUSB=n"
+    "CONFIG_PACKAGE_luci-app-netspeedtest=y"
+    "CONFIG_PACKAGE_luci-app-vlmcsd=y"
+    "CONFIG_COREMARK_OPTIMIZE_O3=y"
+    "CONFIG_COREMARK_ENABLE_MULTITHREADING=y"
+    "CONFIG_COREMARK_NUMBER_OF_THREADS=6"
+    #"CONFIG_PACKAGE_luci-theme-design=y"
+    "CONFIG_PACKAGE_luci-app-filetransfer=y"
+    "CONFIG_PACKAGE_openssh-sftp-server=y"
+    "CONFIG_PACKAGE_luci-app-frpc=m" 
+    "CONFIG_OPKG_USE_CURL=y"
+    "CONFIG_PACKAGE_opkg=y"   
+    "CONFIG_USE_APK=n"
+    "CONFIG_PACKAGE_luci-app-tailscale=y"
+    #"CONFIG_PACKAGE_luci-app-msd_lite=y"
+    "CONFIG_PACKAGE_kmod-usb-net=y"
+    "CONFIG_PACKAGE_kmod-usb-net-rndis=y"
+    "CONFIG_PACKAGE_kmod-usb-net-cdc-ether=y"
+    "CONFIG_PACKAGE_usbutils=y"
+        "CONFIG_PACKAGE_luci-app-diskman=y"
+    "CONFIG_PACKAGE_luci-i18n-diskman-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-docker=y"
+    "CONFIG_PACKAGE_luci-i18n-docker-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-dockerman=y"
+    "CONFIG_PACKAGE_luci-i18n-dockerman-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-alist=y"
+    "CONFIG_PACKAGE_luci-i18n-alist-zh-cn=y"
+    "CONFIG_PACKAGE_fdisk=y"
+    "CONFIG_PACKAGE_parted=y"
+    "CONFIG_PACKAGE_iptables-mod-extra=y"
+    "CONFIG_PACKAGE_ip6tables-nft=y"
+    "CONFIG_PACKAGE_ip6tables-mod-fullconenat=y"
+    "CONFIG_PACKAGE_iptables-mod-fullconenat=y"
+    "CONFIG_PACKAGE_libip4tc=y"
+    "CONFIG_PACKAGE_libip6tc=y"
+    "CONFIG_PACKAGE_luci-app-passwall=y"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_SingBox=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=n"
+    "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Plugin=n"
+    "CONFIG_PACKAGE_htop=y"
+    "CONFIG_PACKAGE_fuse-utils=y"
+    "CONFIG_PACKAGE_tcpdump=y"
+    "CONFIG_PACKAGE_sgdisk=y"
+    "CONFIG_PACKAGE_openssl-util=y"
+    "CONFIG_PACKAGE_resize2fs=y"
+    "CONFIG_PACKAGE_qrencode=y"
+    "CONFIG_PACKAGE_smartmontools-drivedb=y"
+    "CONFIG_PACKAGE_usbutils=y"
+    "CONFIG_PACKAGE_default-settings=y"
+    "CONFIG_PACKAGE_default-settings-chn=y"
+    "CONFIG_PACKAGE_iptables-mod-conntrack-extra=y"
+    "CONFIG_PACKAGE_kmod-br-netfilter=y"
+    "CONFIG_PACKAGE_kmod-ip6tables=y"
+    "CONFIG_PACKAGE_kmod-ipt-conntrack=y"
+    "CONFIG_PACKAGE_kmod-ipt-extra=y"
+    "CONFIG_PACKAGE_kmod-ipt-nat=y"
+    "CONFIG_PACKAGE_kmod-ipt-nat6=y"
+    "CONFIG_PACKAGE_kmod-ipt-physdev=y"
+    "CONFIG_PACKAGE_kmod-nf-ipt6=y"
+    "CONFIG_PACKAGE_kmod-nf-ipvs=y"
+    "CONFIG_PACKAGE_kmod-nf-nat6=y"
+    "CONFIG_PACKAGE_kmod-dummy=y"
+    "CONFIG_PACKAGE_kmod-veth=y"
+    "CONFIG_PACKAGE_automount=y"
+    "CONFIG_PACKAGE_luci-app-frps=y" 
 )
 
-# Path to the .config file
-config_file_path=".config" 
 
-# Append lines to the .config file
+# Append configuration lines to .config
 for line in "${provided_config_lines[@]}"; do
-    echo "$line" >> "$config_file_path"
+    echo "$line" >> .config
 done
 
+make defconfig
 
-PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+#修复文件
+find ./ -name "getifaddr.c" -exec sed -i 's/return 1;/return 0;/g' {} \;
+sed -i '/\/usr\/bin\/zsh/d' package/base-files/files/etc/profile
 
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_RULES="surge"
-	HP_PATCH="homeproxy/root/etc/homeproxy"
+find ./ -name "cascade.css" -exec sed -i 's/#5e72e4/#31A1A1/g; s/#483d8b/#31A1A1/g' {} \;
+find ./ -name "dark.css" -exec sed -i 's/#5e72e4/#31A1A1/g; s/#483d8b/#31A1A1/g' {} \;
+find ./ -name "cascade.less" -exec sed -i 's/#5e72e4/#31A1A1/g; s/#483d8b/#31A1A1/g' {} \;
+find ./ -name "dark.less" -exec sed -i 's/#5e72e4/#31A1A1/g; s/#483d8b/#31A1A1/g' {} \;
 
-	chmod +x ./$HP_PATCH/scripts/*
-	rm -rf ./$HP_PATCH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
-	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
-
-	cd .. && rm -rf ./$HP_RULES/
-
-	cd $PKG_PATCH && echo "homeproxy date has been updated!"
-fi
-
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-
-#
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
-
+#修改ttyd为免密
+install -Dm755 "${GITHUB_WORKSPACE}/patch/99_ttyd-nopass.sh" "package/base-files/files/etc/uci-defaults/99_ttyd-nopass"
+install -Dm755 "${GITHUB_WORKSPACE}/patch/99_set_argon_primary.sh" "package/base-files/files/etc/uci-defaults/99_set_argon_primary"
